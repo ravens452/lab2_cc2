975841e4c45ab9b40dbb3ec2c415e6b6ed8c1b5d
Andrade Guzman <ravens452@gmail.com>
2020-05-09 00:10:42 -0500

    Initial Commit

---
+++ lab_2_CC2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_2_CC2
+//
+//  Created by carlos andrade guzman on 5/9/20.
+//  Copyright © 2020 carlos andrade guzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
