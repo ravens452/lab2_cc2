0fe221d81c160476350bac1ea5fa5b9a48306052
Andrade Guzman <ravens452@gmail.com>
2020-05-10 10:03:41 -0500

    actualizando gitignore

--- lab_2_CC2/Ejercicio_3.cpp
+++ lab_2_CC2/Ejercicio_3.cpp
@@ -7,3 +7,33 @@
 //
 
 //Implementar una función que ordene los elementos de un arreglo: ascendente.
+#include <iostream>
+using namespace std;
+
+
+void ordenarArray(int array[], int tam){
+    int aux;
+    for(int i = 0; i < tam; i++){
+        for(int x = 0; x < tam; x++){
+            if(array[i] < array[x]){
+                aux = array[x];
+                array[x] = array[i];
+                array[i] = aux;
+            }
+        }
+    }
+    cout << "\nArray dinamico ordenado: ";
+    for(int i = 0; i < tam; i++){
+        cout << "{" << array[i] << "} ";
+    }
+    cout << "Fin!" << endl;
+}
+
+
+
+int main(){
+    int arreglo[] = {3,1,8,4,5,9,7};
+    int tam = 7;
+    ordenarArray(arreglo, tam);
+ 
+}
