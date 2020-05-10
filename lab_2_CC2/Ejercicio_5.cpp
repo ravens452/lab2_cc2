b779c5f2c499ab3f2373f44710dbcc2567ab0a3d
Andrade Guzman <ravens452@gmail.com>
2020-05-10 12:54:31 -0500

    Ejercicio 5 resuelta

--- lab_2_CC2/Ejercicio_5.cpp
+++ lab_2_CC2/Ejercicio_5.cpp
@@ -6,5 +6,35 @@
 //  Copyright © 2020 carlos andrade guzman. All rights reserved.
 //
 
+
 //Implemente una función que reciba dos cadenas a y b; dicha función debe concatenar la cadena a a la cadena b.
 //(asuma que la cadena b tiene el tamaño suficiente para contener los elementos de a).
+#include <iostream>
+using namespace std;
+
+int longitud(char cadena[])
+{
+   int acum = 0;
+   while( cadena[acum] != '\0' ) //mientras no sea fin de cadena
+       acum++;
+   return acum;
+}
+void insertarCadena(char    cadenaA[],char cadenaB[]){
+    int tam1 = longitud(cadenaB);
+    int a=0;
+    for (int i = tam1; i<50; i++) {
+        cadenaB[i] = cadenaA[a];
+        a++;
+    }
+    
+}
+
+int c = 0;
+int main(){
+    char a[50];
+    char b[50];
+    cout<<"Escriba la cadena a: "<<endl;cin>>a;
+    cout<<"Escriba la cadena b: "<<endl;cin>>b;
+    insertarCadena(a, b);
+    cout<<"LACADENA UNIDA ES: "<<b<<endl;
+}
